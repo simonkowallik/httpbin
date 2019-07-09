770c7846b3da03254be0f54a46202e656570b5f2
Simon Kowallik <github@simonkowallik.com>
2019-07-09 22:21:18 +0200

    add ecdsa cert+key generation, remove uwsgi buffering for /drip

--- dockerfiles/alpine-nginx/entrypoint.sh
+++ dockerfiles/alpine-nginx/entrypoint.sh
@@ -17,18 +17,27 @@ then
              "keyUsage=critical,digitalSignature,keyEncipherment\n"\
              "subjectAltName=DNS:example.com,DNS:www.example.com\n"\
              "extendedKeyUsage=critical,serverAuth,clientAuth\n" > /tmp/openssl.conf
-
+    # rsa
     openssl req -x509 -newkey rsa:2048 -nodes -sha256 -batch \
             -days 1825 -config /tmp/openssl.conf -extensions ext \
             -keyout /etc/nginx/ssl/key.pem -out /etc/nginx/ssl/cert.pem \
             -subj "/C=AQ/O=Example Corp./CN=www.example.com" > /dev/null 2>&1
-    rm /tmp/openssl.conf
+    # ecdsa
+    if [ ! -f /etc/nginx/ssl/eccert.pem ] && [ ! -f /etc/nginx/ssl/eckey.pem ]
+    then
+      openssl ecparam -name prime256v1 > /tmp/prime256v1.ecparam
+      openssl req -x509 -newkey ec:/tmp/prime256v1.ecparam -nodes -sha256 -batch \
+              -days 1825 -config /tmp/openssl.conf -extensions ext \
+              -keyout /etc/nginx/ssl/eckey.pem -out /etc/nginx/ssl/eccert.pem \
+              -subj "/C=AQ/O=Example Corp./CN=www.example.com" > /dev/null 2>&1
+    fi
+    rm -f /tmp/openssl.conf /tmp/prime256v1.ecparam
   fi
 
   if [ ! -f /etc/nginx/ssl/chain.pem ]
   then
-    echo "/etc/nginx/ssl/chain.pem not found, linking to /etc/nginx/ssl/cert.pem"
-    ln -s /etc/nginx/ssl/cert.pem /etc/nginx/ssl/chain.pem
+    echo "/etc/nginx/ssl/chain.pem not found, copying from /etc/nginx/ssl/cert.pem and /etc/nginx/ssl/eccert.pem"
+    cat /etc/nginx/ssl/cert.pem /etc/nginx/ssl/eccert.pem > /etc/nginx/ssl/chain.pem
   fi
 
   echo "starting uwsgi in background"
